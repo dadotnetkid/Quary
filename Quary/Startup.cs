04d486f2de5ffb5940ab67c62b8f96a3cda08f70
Mark Christopher Cacal <markchristopher.cacal@outlook.com>
2019-04-16 06:26:50 +0800

    Add project files.

---
+++ Quary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quary.Startup))]
+namespace Quary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
